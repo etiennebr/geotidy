3735f293824a29e02d97a587c2cf266c7966626b
Etienne B. Racine <etiennebr@gmail.com>
2019-06-06 12:50:21 -0400

    create line from pairs of points

--- tests/testthat/test-cast.R
+++ tests/testthat/test-cast.R
@@ -2,10 +2,11 @@ context("test-cast")
 
 test_that("`cast_union` returns union of vectors", {
   pts <- st_makepoint(c(1:3), c(3:5))
-  expect_equal(cast_union(pts, pts, "LINESTRING"), st_makeline(pts))
+  expect_equal(cast_union(pts, pts, .cast = "LINESTRING"), st_makeline(pts))
 })
 
 test_that("`cast_combine` works", {
   pts <- st_makepoint(c(1:3), c(3:5))
-  expect_equal(cast_combine(pts, "LINESTRING"), st_makeline(pts))
+  expect_equal(cast_combine(pts, .cast = "LINESTRING"), st_makeline(pts))
+  expect_error(cast_combine(pts, .cast = "LINESTRING", .by_feature = TRUE), "must provide `y`")
 })
