e47cd7fb70ad7a2f88d80b4e9efebd8f167e96e3
Etienne B. Racine <etiennebr@gmail.com>
2019-07-21 07:48:30 -0400

    simplify st_numpoints

--- tests/testthat/test-st_numpoints.R
+++ tests/testthat/test-st_numpoints.R
@@ -6,3 +6,11 @@ test_that("can count number of points", {
   expect_npoints(st_makepoint(list(1:2, 3:4)) %>% st_multi(), 2)
   expect_npoints(st_makepoint(list(1:2, 3:4)) %>% st_makeline(), 2)
 })
+
+test_that("can count number of points of multi polygons", {
+  x <- st_geomfromtext(
+    "MULTIPOLYGON(((77 29,72 26,77 21,77 29), (17 19,12 16,15 15,17 11,17 19)))"
+    )
+  expect_equal(st_numpoints(x), 9)
+  expect_equal(st_numpoints(c(x, x)), c(9, 9))
+})
